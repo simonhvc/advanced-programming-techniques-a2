d47624aedee436630184f8c8585fac377c50744c
s3717110 <41350103+s3717110@users.noreply.github.com>
2019-05-10 09:14:38 +1000

    Update Player.cpp

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,1 +1,15 @@
-
+#include <string>
+class Player { 
+    std::string name, studentNumber, Email;
+    int score;
+    Hand hand;
+  
+    // Default Constructor 
+    Player(std::string name, std::string studentNumber, std::string Email) 
+    { 
+      this->name = name;
+      this->studentNumber = studentNumber;
+      this->Email = Email;
+    } 
+  
+}; 
