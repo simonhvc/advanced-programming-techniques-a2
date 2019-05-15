891d970d39c34b833fda50591f3cd2b71642f13e
s3717110 <41350103+s3717110@users.noreply.github.com>
2019-05-15 19:40:15 +1000

    Update LinkedList.cpp

--- source/LinkedList.cpp
+++ source/LinkedList.cpp
@@ -22,3 +22,54 @@ void LinkedList::add(Tile tile){
     this->length++;
 }
 
+void LinkedList::print(){
+    Node* head = this->head;
+    int i = 1;
+    while(head){
+        std::cout << i << ": " << head->tile << std::endl;
+        head = head->next;
+        i++;
+    }
+}
+
+void LinkedList::deleteNode(Tile tile){
+   if(head->next == NULL)  
+        {  
+            cout << "There is only one node." << 
+                    " The list can't be made empty ";  
+            return;  
+        }  
+  
+        /* Copy the data of next node to head */
+        head->data = head->next->data;  
+  
+        // store address of next node  
+        n = head->next;  
+  
+        // Remove the link of next node  
+        head->next = head->next->next;  
+  
+        // free memory  
+        free(n);  
+  
+        return;  
+        }
+   Node *prev = head;  
+    while(prev->next != NULL && prev->next != n)  
+        prev = prev->next;  
+  
+    // Check if node really exists in Linked List  
+    if(prev->next == NULL)  
+    {  
+        cout << "\nGiven node is not present in Linked List";  
+        return;  
+    }  
+  
+    // Remove node from Linked List  
+    prev->next = prev->next->next;  
+  
+    // Free memory  
+    free(n);  
+  
+    return;  
+}
