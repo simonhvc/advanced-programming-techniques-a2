9af2740335ce2281fafdf57c205fd9e36e61518e
Simon Hesjevik <s3694451@student.rmit.edu.au>
2019-05-22 20:23:28 +1000

    Updated gameloop

--- source/Menu.cpp
+++ source/Menu.cpp
@@ -131,14 +131,16 @@ void Menu::gameplayLoop() {
   //The gameplay loop
   while(loop) {
 
-    std::string response = "";
-    bool hasPlacedTile = false;
-    Tile* tileToPlace = nullptr;
-    int yPos = 0;
-    int xPos = 0;
-    int score = 0;
+
 
     for(int i = 0; i < 2; i++) {
+
+      std::string response = "";
+      bool hasPlacedTile = false;
+      Tile* tileToPlace = nullptr;
+      int yPos = 0;
+      int xPos = 0;
+      int score = 0;
 
 
       //Gets the player command
@@ -211,7 +213,7 @@ void Menu::gameplayLoop() {
         }
 
         //Gives score if placement is valid
-        if (score != -1) {
+        if (score > 0) {
           hasPlacedTile = true;
           players[i]->updateScore(score);
         }
